c2a9e07f577bf1155561f6f548006480caa78ca8
naji7 <nasik7@ymail.com>
2025-09-06 01:28:21 +0530

    add cluster

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import cluster from "cluster";
+import os from "os";
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -5,14 +7,35 @@ import { createApp } from "./app";
 
 let PORT = process.env.PORT || "3000";
 
-const main = async () => {
+if (cluster.isPrimary) {
+  const numOfCPUs = os.cpus().length;
+  console.log(`Master process PID: ${process.pid}`);
+  console.log(`Forking ${numOfCPUs} workers...`);
+
+  for (let i = 0; i < numOfCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on("exit", (worker, code, signal) => {
+    console.log(`Worker ${worker.process.pid} died. Forking a new worker!`);
+    cluster.fork();
+  });
+} else {
   const app = createApp();
 
   app.listen(parseInt(PORT), () => {
     console.log(`Server started on port ${PORT}`);
   });
-};
+}
 
-main().catch((err) => {
-  console.log(err.message);
-});
+// const main = async () => {
+//   const app = createApp();
+
+//   app.listen(parseInt(PORT), () => {
+//     console.log(`Server started on port ${PORT}`);
+//   });
+// };
+
+// main().catch((err) => {
+//   console.log(err.message);
+// });
